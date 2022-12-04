774f2a6ff3bef9a1434e595a3b8b9ca68ed0b0a4
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-12-04 12:54:38 +0000

    Add drop down menu for small screens, change nav on scroll, get links working

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const contactSection = document.querySelector(".contact")
 const seeWorkButtonHero = document.querySelector(".hero-section button")
 const portfolioSection = document.querySelector(".wrapper-test")
 const whatWeDoContactButton = document.querySelector(".what-we-do-left button")
+console.log(contactButtonHeader)
+
 contactButtonHeader.addEventListener("click", (e) => {
     contactSection.scrollIntoView()
+    console.log("hello")
 })
 
 seeWorkButtonHero.addEventListener("click", (e) => {
@@ -14,4 +17,60 @@ seeWorkButtonHero.addEventListener("click", (e) => {
 whatWeDoContactButton.addEventListener("click", (e => {
     contactSection.scrollIntoView()
 }))
+
+/****************************************/
+const header = document.querySelector("header")
+const nav = document.querySelectorAll("nav ul li")
+const hero = document.querySelector(".hero-section")
+const paragraphSection = document.querySelector(".paragraph-section")
+contactButtonHeader 
+
+const sectionOneOptions = { }
+const paragraphSectionOptions = { }
+
+
+
+
+/***********************************************/
+const menu = document.querySelector(".menu")
+const dropDown = document.querySelector(".drop-down")
+
+menu.addEventListener("click", (e) => {
+    dropDown.classList.toggle("toggle")
+})
+
+const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver) {
+    entries.forEach(entry => {
+        console.log(entry.target)
+        header.classList.toggle("header-add")
+        nav.forEach(link => {
+            link.classList.toggle("nav-add")
+        })
+        dropDown.classList.toggle("drop-down-add")
+        contactButtonHeader.classList.toggle("button-add")
+    })
+}, sectionOneOptions)
+sectionOneObserver.observe(hero)
+
+
+/**********************************/
+
+const ourWorkNav = document.querySelector(".our-work-nav")
+const homeNav = document.querySelector(".nav-add")
+const pricingNav = document.querySelector(".pricingNav")
+const pricingSection = document.querySelector(".pricing-section")
+
+ourWorkNav.addEventListener("click", (e) => {
+    portfolioSection.scrollIntoView()
+})
+
+homeNav.addEventListener("click", (e) => {
+    hero.scrollIntoView()
+})
+
+pricingNav.addEventListener("click", (e) => {
+    pricingSection.scrollIntoView()
+})
+
+
 
