43dc68367854ecfad2b813d2af84396a9dc6f628
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-12-04 23:45:25 +0000

    Add portfolio page and change gap in form

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+const seeWorkButton = document.querySelector(".seeWorkButton")
+const portfolioScroll = document.querySelector(".spline")
+const ciLightingNav = document.querySelector(".why-us-nav")
+
+seeWorkButton.addEventListener("click", (e) => {
+    portfolioScroll.scrollIntoView()
+})
+ciLightingNav.addEventListener("click", (e) => {
+    portfolioScroll.scrollIntoView()
+})
