18f4fc5b1bbb87de47886ce1fcc5beb0a39b59a3
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-12-12 14:50:03 +0000

    Change portfolio hero section and add media queries

--- web-design/portfolio.js
+++ web-design/portfolio.js
@@ -1,1 +1,12 @@
 
+/***********************************************/
+const menu = document.querySelector(".menu")
+const dropDown = document.querySelector(".drop-down")
+const ourWorkNav = document.querySelector(".our-work-nav")
+const whyUsNav = document.querySelector(".why-us-nav")
+const whyUsSection = document.querySelector(".why-us")
+
+
+menu.addEventListener("click", (e) => {
+    dropDown.classList.toggle("toggle")
+})
