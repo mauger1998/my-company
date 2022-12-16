6054555606213c57440aa2986f68d513bf6f65a1
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-12-16 11:01:16 +0000

    Add functionality to the thumbnails

--- web-design/portfolio.js
+++ web-design/portfolio.js
@@ -1,5 +1,4 @@
 
-/***********************************************/
 const menu = document.querySelector(".menu")
 const dropDown = document.querySelector(".drop-down")
 const ourWorkNav = document.querySelector(".our-work-nav")
@@ -9,4 +8,21 @@ const whyUsSection = document.querySelector(".why-us")
 
 menu.addEventListener("click", (e) => {
     dropDown.classList.toggle("toggle")
+})
+
+/***********************************************/
+
+const skipToProjectsButton = document.querySelector(".hero-section button")
+const projectThumbnails = document.querySelector(".portfolio-thumbnails")
+const gridItemThumbnailArray = Array.from(document.querySelectorAll(".grid-item"))
+const projectArray = document.querySelectorAll(".cilighting")
+
+skipToProjectsButton.addEventListener("click", (e) => {
+    projectThumbnails.scrollIntoView()
+})
+
+gridItemThumbnailArray.forEach((thumbnail, index) => {
+    thumbnail.addEventListener("click", (e) => {
+        projectArray[index].scrollIntoView()
+    })
 })
