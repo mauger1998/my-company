28dc904c6a170e4790fd8bf9bd4a26b081b9ebf0
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-12-18 14:05:22 +0000

    Add header and drop downs to new pricing page

---
+++ pricing/pricing.js
@@ -0,0 +1,95 @@
+
+const menu = document.querySelector(".menu")
+const dropDown = document.querySelector(".drop-down")
+const ourWorkNav = document.querySelector(".our-work-nav")
+const whyUsNav = document.querySelector(".why-us-nav")
+const whyUsSection = document.querySelector(".why-us")
+
+
+menu.addEventListener("click", (e) => {
+    dropDown.classList.toggle("toggle")
+})
+
+
+
+
+
+
+
+/****************************************/
+const burgerOurWorkDrop = document.querySelector(".burger-our-work-drop")
+const burgerOurWorkMenu = document.querySelector(".drop-down-our-work-burger")
+
+burgerOurWorkDrop.addEventListener("click", (e) => {
+    burgerOurWorkMenu.classList.toggle("drop-down-burger-add")
+})
+
+const burgerWebDesignDrop = document.querySelector(".burger-web-design-drop")
+const burgerWebDesignMenu = document.querySelector(".drop-down-web-design-burger")
+
+burgerWebDesignDrop.addEventListener("click", (e) => {
+    burgerWebDesignMenu.classList.toggle("drop-down-burger-add")
+})
+
+const burgepricingDrop = document.querySelector(".burger-pricing-drop")
+const burgepricingMenu = document.querySelector(".drop-down-pricing-burger")
+
+burgepricingDrop.addEventListener("click", (e) => {
+    burgepricingMenu.classList.toggle("drop-down-burger-add")
+})
+
+
+
+/***********************/
+
+const ourWorkMenu = document.querySelector(".our-work-menu") 
+const ourWorkLink = document.querySelector(".our-work-drop")
+
+const ourWorkMenuP = document.querySelectorAll(".our-work-menu p")
+const webDesignMenuP = document.querySelectorAll(".web-design-menu p")
+const pricingMenuP = document.querySelectorAll(".pricing-menu p")
+
+
+
+ourWorkLink.addEventListener("click", (e) => {
+    ourWorkMenu.classList.toggle("add-to-drop")
+    ourWorkMenuP.forEach(p => {
+        p.classList.toggle("cursor")
+    })
+    
+})
+
+
+
+
+const webDesignMenu = document.querySelector(".web-design-menu") 
+const webDesignLink = document.querySelector(".web-design-drop")
+
+webDesignLink.addEventListener("click", (e) => {
+    webDesignMenu.classList.toggle("add-to-drop")
+    webDesignMenuP.forEach(p => {
+        p.classList.toggle("cursor")
+    })
+   
+    
+})
+
+
+
+const pricingMenu = document.querySelector(".pricing-menu") 
+const pricingLink = document.querySelector(".pricing-drop")
+
+pricingLink.addEventListener("click", (e) => {
+    pricingMenu.classList.toggle("add-to-drop")
+    pricingMenuP.forEach(p => {
+        p.classList.toggle("cursor")
+    })
+
+   
+    
+})
+
+
+
+
+
